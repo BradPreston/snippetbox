76a9232ff2740499270b97839d510f651b6e4611
bradpreston <bap5393@gmail.com>
2023-06-17 14:48:14 -0400

    Add proper error handing and logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,42 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type config struct {
+    addr string
+    staticDir string
+}
+
+type application struct {
+    errorLog *log.Logger
+    infoLog *log.Logger
+}
+
 func main() {
-	mux := http.NewServeMux()
+    var cfg config 
+    flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+    flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+    flag.Parse()
 
-    fileServer := http.FileServer(http.Dir("./ui/static/"))
-    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+    infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+    errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+    app := &application{
+        errorLog: errorLog,
+        infoLog: infoLog,
+    }
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+    srv := &http.Server{
+        Addr: cfg.addr,
+        ErrorLog: errorLog,
+        Handler: app.routes(),
+    }
+	infoLog.Printf("Starting server on %s", cfg.addr)
+    err := srv.ListenAndServe()
+    errorLog.Fatal(err)
 }
